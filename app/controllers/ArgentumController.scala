e1fd437d04711da86eb3160d3b416b8b0a74a586
Cristianospsp <cristianospsp@gmail.com>
2016-07-29 18:38:18 -0300

    controller argentum

---
+++ app/controllers/ArgentumController.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import java.time.LocalDateTime
+import javax.inject.{Inject, Singleton}
+import br.com.caelum.argentum.model.Negotiation
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.{JsValue, Json, Writes}
+import play.api.mvc.{Action, Controller}
+
+
+/**
+ * Created by Cristiano on 09/06/16.
+ */
+@Singleton
+class ArgentumController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  implicit val implicitNegocicaoWrites = new Writes[Negotiation] {
+    override def writes(o: Negotiation): JsValue = {
+      Json.obj(
+        "price" -> o.price,
+        "amount" -> o.amount,
+        "date" -> o.date.toString
+      )
+    }
+  }
+
+  def negociacoes = Action {
+    val today = LocalDateTime.now()
+    val n = Negotiation(BigDecimal(40.5), 100, today)
+    val n2 = Negotiation(BigDecimal(44.5), 100, today)
+    val n3 = Negotiation(BigDecimal(55.5), 100, today)
+    val negs = List(n, n2, n3)
+    val json = Json.toJson(negs)
+    Ok(views.html.teste(negs))
+  }
+
+}
